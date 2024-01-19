f086a2ac08a49aee8040c4083fe27b54ceb4919d
Ido Toker <idoto@post.bgu.ac.il>
2024-01-19 14:04:52 +0200

    changes

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -7,14 +7,29 @@ int numOrders;
 Customer::Customer(int id, const string &name, int locationDistance, int maxOrders) : id(0), &name(),locationDistance(2), maxOrders(3) {
     numOrders = 0;
 }
-const string Customer:: &getName() const {return &name;};
-int Customer:: getId() const {return id;};
-int Customer:: getCustomerDistance() const {return locationDistance;};
-int Customer:: getMaxOrders() const {return maxOrders;};
-int Customer:: getNumOrders() const {return numOrders;};
-bool Customer:: canMakeOrder() const {
-    if(maxOrders > numOrders) {return true;}
-    else {return false;}
-};
+const string& Customer::getName() const
+{
+    return name;
+}
+int Customer:: getId() const
+{
+    return id;
+}
+int Customer:: getCustomerDistance() const
+{
+    return locationDistance;
+}
+int Customer:: getMaxOrders() const
+{
+    return maxOrders;
+}
+int Customer:: getNumOrders() const
+{
+    return numOrders;
+}
+bool Customer:: canMakeOrder() const
+{
+    return maxOrders > numOrders;
+}
 //vector<int> Customer:: &getOrders() const;
 // int addOrder(int orderId); 
--- src/Volunteer.cpp
+++ src/Volunteer.cpp
@@ -11,9 +11,42 @@ const string &Volunteer::getName() const
 {
     return name;
 }
-int Volunteer::getActiveOrderId() const {return activeOrderId;}
-int Volunteer::getCompletedOrderId() const {}
-bool Volunteer::isBusy() const 
+int Volunteer::getActiveOrderId() const 
+{
+    return activeOrderId; 
+}
+int Volunteer::getCompletedOrderId() const 
+{
+    return completedOrderId;
+}
+bool Volunteer::isBusy() const
 {
     return activeOrderId != NO_ORDER;
+}
+// do i need to do something with the virtual funcs?
+
+
+CollectorVolunteer::CollectorVolunteer(int id, string name, int coolDown): Volunteer(id, name), coolDown(coolDown){}
+CollectorVolunteer *CollectorVolunteer::clone() const{} //override?
+void CollectorVolunteer::step(){} //override?
+int CollectorVolunteer::getCoolDown() const
+{
+    return coolDown;
+}
+int CollectorVolunteer::getTimeLeft() const
+{
+    return timeLeft;
+}
+bool CollectorVolunteer::decreaseCoolDown()
+{
+    --timeLeft;
+    return timeLeft == 0;
+}
+bool CollectorVolunteer::hasOrdersLeft() const
+{
+    return true;
+}
+bool CollectorVolunteer::canTakeOrder(const Order &order) const
+{
+    
 }
