4dd682e884106e3769e88fef09d35d901adf7dc1
Ido Toker <idoto@post.bgu.ac.il>
2024-01-19 13:06:39 +0200

    changes

--- src/Volunteer.cpp
+++ src/Volunteer.cpp
@@ -1,15 +1,19 @@
 #include "../include/Volunteer.h"
 #include "../include/WareHouse.h"
+#include "../include/Order.h"
 
- bool isBusy() const;
-
-Volunteer:: Volunteer(int id(0), const string &name()){
-    isBusy(false);
-    
+Volunteer::Volunteer(int id, const string &name) : id(id), name(name), completedOrderId(NO_ORDER), activeOrderId(NO_ORDER) {}
+int Volunteer::getId() const
+{
+    return id;
 }
-
-int Volunteer:: getId() const {return id;};
-const string Volunteer:: &getName() const {return &name;};
-
-
-
+const string &Volunteer::getName() const
+{
+    return name;
+}
+int Volunteer::getActiveOrderId() const {return activeOrderId;}
+int Volunteer::getCompletedOrderId() const {}
+bool Volunteer::isBusy() const 
+{
+    return activeOrderId != NO_ORDER;
+}
